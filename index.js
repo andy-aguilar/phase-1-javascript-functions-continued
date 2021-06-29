38e06d480d5fcfb5c6f72d31e2bb3ba4f0394e16
andy-aguilar <andres.aguilar@flatironschool.com>
2021-06-29 11:55:58 -0400

    commit tree 2ed9d227c4ce71d8696b400d123096adddd440f7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair="*"){
+    return (adj="Special") => `You are ${flair + adj + flair}!`
+}
